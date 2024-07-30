45e778706058e77f5baf5ffed0b74cdbc7fb12e4
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-07-30 14:04:42 -0700

    main h file

--- main.h
+++ main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef struct printHandler
+
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
