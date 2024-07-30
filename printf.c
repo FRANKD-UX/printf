d4bfdf5640346545abad37c941223af6313338df
FRANKD-UX <frankdafrica@gmail.com>
2024-07-30 13:36:16 -0700

    2

---
+++ printf.c
@@ -0,0 +1,75 @@
+#include <stdarg.h>
+#include <unistd.h> /* For the write function */
+#include <stddef.h> /* For NULL */
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: On success, 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - Writes a string to stdout
+ * @str: The string to write
+ *
+ * Return: The number of characters written.
+ */
+int _puts(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string containing the characters and the specifiers.
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
+				count += _puts(va_arg(args, char *));
+			else if (format[i] == '%')
+				count += _putchar('%');
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+			count += _putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
+
